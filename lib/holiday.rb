b35915c3e24e8c03dd028bec170928ba0806b5af
Mikelanne Wood <mikelanne.wood@gmail.com>
2020-09-21 14:24:06 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -75,6 +75,7 @@ def all_holidays_with_bbq(holiday_hash)
    holiday.map do |holiday, supplies|
     if supplies.include? ("BBQ")
       holiday
+    elsif supplies.include? ("BBQ")
     end
    end 
  end
