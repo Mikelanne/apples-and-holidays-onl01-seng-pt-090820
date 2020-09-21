888b06215fcab232778c1870730cbdfc86de47cf
Mikelanne Wood <mikelanne.wood@gmail.com>
2020-09-21 14:04:48 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -71,10 +71,10 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
- # holiday_hash.each do |season, data|
-   # data.each do |holiday, supplies|
-    #  if supplies.include? ("BBQ")
-     #   puts 
+ holiday_hash.each do |season, data|
+   data.each do |holiday, supplies|
+    if supplies.include? ("BBQ")
+     puts 
 end
 
 
